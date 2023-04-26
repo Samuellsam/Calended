4478af1e24917f379f25a1bfedba1e02c5e3c085
Samuel <u072972@bca.co.id>
2023-04-26 11:51:19 +0700

    removed unused style

--- src/pages/components/DayView.tsx
+++ src/pages/components/DayView.tsx
@@ -2,8 +2,8 @@ const DayCalendarView: React.FC<{
   date: number;
 }> = (props) => {
   return (
-    <div className="bg-teal-100 p-4 day-view rounded-lg">
-      <p className="text-teal-600 font-bold">{props.date}</p>
+    <div className="p-4 day-view rounded-lg">
+      <p className="font-bold">{props.date}</p>
     </div>
   );
 };
