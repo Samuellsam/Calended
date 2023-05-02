62938ee4b7eb5095987da5d56c2e6d7f46ebd31f
Samuel <u072972@bca.co.id>
2023-05-02 17:39:28 +0700

    changed wfo team text logic

--- src/enums/WfhTeamEnum.ts
+++ src/enums/WfhTeamEnum.ts
@@ -7,12 +7,10 @@ export enum WfhTeamEnum {
 
 export const getWfoTeam = (wfhTeam: WfhTeamEnum) => {
   let wfo = "";
-
   let step = Object.values(WfhTeamEnum).indexOf(wfhTeam);
-
-  if (step + 1 > 3) step = 0;
-
   let size = Object.values(WfhTeamEnum).length;
+
+  if (step >= size) step = 0;
 
   for (let i = 0; i < size; i++, step++) {
     if (Object.values(WfhTeamEnum)[step] === wfhTeam) continue;
