8fe819b169ac8ecfa8e9020fa4b1d8ab5c16d2d0
Samuel <u072972@bca.co.id>
2023-05-08 14:51:35 +0700

    added title

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -51,6 +51,9 @@ const HomePage: React.FC<{}> = () => {
 
   return (
     <div>
+      <p className="font-caveat-700 text-slate-100 mx-auto mt-3 w-max drop-shadow-[0_20px_20px_rgba(255,255,255,1)]">
+        {"Calended~"}
+      </p>
       <div className="day-view-container flex flex-row flex-wrap justify-start mx-auto">
         {generateDayHeader()}
       </div>
