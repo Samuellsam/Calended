5835a84c374544463c6ed1a9483776e8a39627b7
Samuel <u072972@bca.co.id>
2023-07-28 18:00:01 +0700

    Added icon to menu item

--- src/components/menu/MainMenu.tsx
+++ src/components/menu/MainMenu.tsx
@@ -1,5 +1,11 @@
 import { MenuEnum } from "@/enums/MenuEnum";
 import MenuItem from "./MenuItem";
+import {
+  faBook,
+  faCalendarTimes,
+  faPeopleGroup,
+  faPeopleRobbery,
+} from "@fortawesome/free-solid-svg-icons";
 
 const MainMenu: React.FC<{
   onMenuSelect: (menu: MenuEnum) => void;
@@ -8,21 +14,25 @@ const MainMenu: React.FC<{
     <>
       <div className="grid grid-cols-3">
         <MenuItem
+          icon={faPeopleGroup}
           name="Team"
           onClick={() => props.onMenuSelect(MenuEnum.TEAM)}
-        />
+        ></MenuItem>
         <MenuItem
           name="Off Day"
+          icon={faCalendarTimes}
           onClick={() => props.onMenuSelect(MenuEnum.OFF_DAY)}
-        />
+        ></MenuItem>
         <MenuItem
           name="Member"
+          icon={faPeopleRobbery}
           onClick={() => props.onMenuSelect(MenuEnum.MEMBER)}
-        />
+        ></MenuItem>
         <MenuItem
           name="Base Date"
           onClick={() => props.onMenuSelect(MenuEnum.BASE_DATE)}
-        />
+          icon={faBook}
+        ></MenuItem>
       </div>
     </>
   );
--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -1,13 +1,23 @@
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const MenuItem: React.FC<{
   name: string;
   onClick?: () => void;
+  icon: IconDefinition;
 }> = (props) => {
   return (
     <div
       className="relative mx-auto menu-item bg-slate-500 hover:bg-slate-400 text-slate-100 cursor-pointer shadow-md rounded-lg"
       onClick={props.onClick}
     >
-      <div className="flex grow"></div>
+      <div className="flex">
+        <FontAwesomeIcon
+          icon={props.icon}
+          size="4x"
+          className="m-auto mt-4"
+        ></FontAwesomeIcon>
+      </div>
       <p className="menu-item-text absolute font-bold left-1/2 transform -translate-x-1/2 bottom-3 text-center menu-text text-slate-100">
         {props.name}
       </p>
